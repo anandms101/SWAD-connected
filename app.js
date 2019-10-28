b8c96b2b2f217845f5fbfbc810eb7592bc9fe304
Anand MS <anandmohan.singh101@gmail.com>
2019-10-29 05:10:08 +0530

    connected all pages

--- app.js
+++ app.js
@@ -9,29 +9,63 @@ app.get("/", function(req, res){
     res.render("landing");
 });
 
+app.post("/",function(req,res)
+{
+    res.redirect("/");
+});
 
 app.get("/chinese", function(req, res){
     res.render("chinese");
+});
+
+app.post("/chinese",function(req,res)
+{
+    res.redirect("/chinese");
 });
 
 app.get("/contactus", function(req, res){
     res.render("contactus");
 });
 
+app.post("/contactus",function(req,res)
+{
+    res.redirect("/contactus");
+});
+
 app.get("/LogIn", function(req, res){
     res.render("LogIn");
+});
+
+app.post("/LogIn",function(req,res)
+{
+    res.redirect("/LogIn");
 });
 
 app.get("/north", function(req, res){
     res.render("north");
 });
 
+app.post("/north",function(req,res)
+{
+    res.redirect("/north");
+});
+
 app.get("/signup", function(req, res){
     res.render("signup");
 });
 
+app.post("/signup",function(req,res)
+{
+    res.redirect("/signup");
+});
+
 app.get("/south", function(req, res){
     res.render("south");
+});
+
+app.post("/south",function(req,res)
+{
+    res.redirect("/south");
 });
 
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
