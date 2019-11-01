43c41237aa9dd666234b0936f9660b0ebbb238f8
Anand MS <anandmohan.singh101@gmail.com>
2019-11-01 11:25:33 +0530

    css files loc change

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 
+app.use(express.static('public'));
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
